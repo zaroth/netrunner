084106209b6e8cde9fe240450b7394ad1fea324b
Lukasz Dobrogowski <lukasz.dobrogowski@gmail.com>
2016-03-31 22:39:52 +0200

    Admin panel updates

--- src/cljs/netrunner/admin.cljs
+++ src/cljs/netrunner/admin.cljs
@@ -4,7 +4,7 @@
             [sablono.core :as sab :include-macros true]
             [netrunner.ajax :refer [GET]]))
 (def editors ["mtgred" "nealterrell" "Saintis" "zaroth" "JoelCFC25" "queueseven"
-              "mediohxcore" "d1en's cat" "Apex"])
+              "mediohxcore" "d1en's cat" "Apex" "Azogar"])
 
 (defn- runner-winrates []
   [["Whizzard: Master Gamer" (+ 80 (rand 10))]
@@ -24,7 +24,22 @@
 
 (defn- connect-settings []
   {:disconnect (zero? (rand-int 2))
-   :blame (zero? (rand-int 2))})
+   :blame (zero? (rand-int 2))
+   :gg (zero? (rand-int 2))
+   :concedes (zero? (rand-int 2))
+   :igdecks (zero? (rand-int 2))})
+
+(defn- community-stats []
+  {
+   :octgn (+ 5 (rand-int 30))
+   :nrdb (+ 100 (rand-int 200))
+   :skill (+ 50 (rand-int 30))
+   })
+
+(defn update-community-stats [owner]
+  (om/set-state! owner [:community-stats :octgn] (+ (rand-int 2) (om/get-state owner [:community-stats :octgn])))
+  (om/set-state! owner [:community-stats :nrdb] (+ (rand-int 10) (om/get-state owner [:community-stats :nrdb])))
+  (om/set-state! owner [:community-stats :skill] (+ (rand-int 5) (om/get-state owner [:community-stats :skill]))))
 
 (defn- new-state []
   {:runner-winrates (runner-winrates)
@@ -32,7 +47,18 @@
    :rng (rng-settings)
    :connect (connect-settings)
    :editor (first (shuffle editors))
-   :spamming ""})
+   :community-stats (community-stats)
+   :spamming {:increment 0 :text [:p ""]}})
+
+(defn increment-spam [owner](om/set-state! owner [:spamming :text] "Assembling bad cards...")
+  (om/set-state! owner [:spamming :increment] (inc (om/get-state owner [:spamming :increment])))
+  (om/set-state! owner [:spamming :text] (let [i (om/get-state owner [:spamming :increment])]
+                                           (list
+                                           [:p "Assembling bad cards... " (when (< 0 i) "Done.")]
+                                           (when (< 1 i) [:p "Writing a novel for description... " (when (< 2 i) "Done.")])
+                                           (when (< 2 i) [:p "Bribing Alsciende... " (when (< 3 i) "Done.")])
+                                           (when (< 3 i) [:p "Spamming likes... " (when (< 4 i) "Done.")])
+                                           (when (< 4 i) [:p "Congratulations, '" (om/get-state owner :decklist-link) "' is the new deck of the week on NRDB!"])))))
 
 (def app-state (atom (new-state)))
 
@@ -48,8 +74,9 @@
           (om/set-state! owner :corp-winrates (corp-winrates))
           (om/set-state! owner :runner-winrates (runner-winrates))
           (om/set-state! owner :rng (rng-settings))
+          (update-community-stats owner)
           (om/set-state! owner :connect (connect-settings)))
-        10000))
+        5000))
     om/IInitState
     (init-state [this]
       (new-state))
@@ -58,21 +85,12 @@
     (render-state [this state]
       (sab/html
       [:div.admin.panel.blue-shade
-       [:h1 "Administration"]
+       [:h1 "Admin panel"]
 
-       [:p "Currently being edited by " [:b {:style {:color "red"}} (om/get-state owner :editor)]]
-       [:h2 "Winrate reports"]
-       [:h3 "Runner"]
-       [:ol
-        (for [run (sort-by second > (om/get-state owner :runner-winrates))]
-          [:li (str (first run) " - " (second run) "%")])]
-       [:h3 "Corp"]
-       [:ol
-        (for [run (sort-by second > (om/get-state owner :corp-winrates))]
-          [:li (str (first run) " - " (second run) "%")])]
+       [:p "Currently being controlled by: " [:b {:style {:color "red"}} (om/get-state owner :editor)]]
 
        [:h2 "RNG settings"]
-       [:p "CapAp starting hand agenda density: "
+       [:p "CapAp's corp starting hand agenda density: "
         (do (for [option [0 1 2 3 4 5]]
           [:label [:input {:type "radio"
                            :style {:margin-left "15px"}
@@ -83,19 +101,24 @@
        [:p [:input {:disabled true :type "checkbox" :checked (om/get-state owner [:rng :snipe])}
             "Snipe only agenda during HQ single access"]]
        [:p [:input {:disabled true :type "checkbox" :checked (om/get-state owner [:rng :siphon])}
-            "Force Account Siphon in opening hand"]]
+            "Force Account Siphon in runner's opening hand"]]
 
-       [:h2 "Connection settings"]
+       [:h2 "Player behavior manipulation"]
        [:p [:input {:disabled true :type "checkbox" :checked (om/get-state owner [:connect :disconnect])}
             "Fake disconnect when losing"]]
        [:p [:input {:disabled true :type "checkbox" :checked (om/get-state owner [:connect :blame])}
-            "Blame my terrible Internet connection on Weyland.meat"]]
-
+            "Blame terrible Internet connection on Weyland.meat servers"]]
+       [:p [:input {:disabled true :type "checkbox" :checked (om/get-state owner [:connect :gg])}
+            "Hide 'gg' typed by players"]]
+       [:p [:input {:disabled true :type "checkbox" :checked (om/get-state owner [:connect :concedes])}
+            "Show concedes as ragequits"]]
+       [:p [:input {:disabled true :type "checkbox" :checked (om/get-state owner [:connect :igdecks])}
+            "Add AI bots to lobby playing museum IG decks"]]
 
        [:h2 "Latest decklists"]
        [:h4 "Dan D'Argenio"]
-       [:ul
-        [:li "For mtgred's eyes only."]]
+       [:ol
+        [:li [:i "For mtgred's eyes only."]]]
 
        [:h4 "Chill84"]
        [:ol
@@ -136,20 +159,32 @@
         [:li "3x Self-modifying Code"]
         [:li "What do you mean, this isn't legal anymore?"]]
 
-       [:h2 "NRDB DotW spambot"]
-       [:p "Enter decklist link: " [:input {:type "text" :style {:width "500px"}
+       [:h2 "NRDB Deck of the Week spambot"]
+       [:p "Enter deck name: " [:input {:type "text" :style {:width "500px"}
                                             :value (om/get-state owner :decklist-link)
                                             :on-change #(om/set-state! owner :decklist-link (.. % -target -value))}]]
-       [:p [:button {:type "button" :on-click #(when (clojure.string/starts-with? (om/get-state owner :decklist-link) "http://")
-                                                (om/set-state! owner :spamming "Spamming likes...")
-                                                (js/setTimeout
-                                                  (fn []
-                                                    (om/set-state! owner :spamming
-                                                                   "Spamming likes.... Done. Congratulations, next week's Deeklist of the Week winner!"))
-                                                  4000))}
-            "Spam Likes"]
-        [:label {:style {:padding-left "10px"}} (om/get-state owner :spamming)]]
+       [:p [:button {:type "button" :on-click #((om/set-state! owner [:spamming :text] [:p "Assembling bad cards..."])
+                                                (om/set-state! owner [:spamming :increment] 0)
+                                                (js/setInterval
+                                                  (fn [] (increment-spam owner))
+                                                  3000))}
+            "Promote a deck"]
+        (om/get-state owner [:spamming :text])]
 
+       [:h2 "Community statistics"]
+       [:p "People wanting to just go back to OCTGN: " (om/get-state owner [:community-stats :octgn])]
+       [:p "Complaints about NRDB deck of the week: " (om/get-state owner [:community-stats :nrdb])]
+       [:p "Complaints about low skill level on weyland.meat: " (om/get-state owner [:community-stats :skill])]
+
+       [:h2 "Winrate reports"]
+       [:h3 "Runner"]
+       [:ol
+        (for [run (sort-by second > (om/get-state owner :runner-winrates))]
+          [:li (str (first run) " - " (second run) "%")])]
+       [:h3 "Corp"]
+       [:ol
+        (for [run (sort-by second > (om/get-state owner :corp-winrates))]
+          [:li (str (first run) " - " (second run) "%")])]
 
        [:h2 "Run Last Click safety word"]
        [:p [:a {:href "https://www.youtube.com/watch?v=-FaoCXTSUQc"} "PERSEPHONE"]]]))))
